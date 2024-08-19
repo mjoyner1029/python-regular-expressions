965d899c26280e730cd627240d18db5c6714ff9e
Miles Joyner <62813978+mjoyner1029@users.noreply.github.com>
2024-08-19 16:11:12 -0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,8 +3,8 @@ import re
 text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
 exclude_domain = "exclude.com"
 
-# Construct the regex pattern to exclude email addresses from 'exclude_domain'
-pattern = r'\b[A-Za-z0-9._%+-]+@(?!{})[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'.format(re.escape(exclude_domain))
+# Use f-string to correctly format the regex pattern
+pattern = rf'\b[A-Za-z0-9._%+-]+@(?!{re.escape(exclude_domain})\b[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
 
 # Find all email addresses except those from 'exclude_domain'
 emails = re.findall(pattern, text)
